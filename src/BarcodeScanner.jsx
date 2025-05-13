1d962b7aab804b0a3eac8152c3707ab0b386b92b
crsto22 <arturo200512@gmail.com>
2025-05-13 16:08:21 -0500

    first commit

--- src/BarcodeScanner.jsx
+++ src/BarcodeScanner.jsx
@@ -1,24 +1,45 @@
-import React, { useState } from 'react';
-import BarcodeScannerComponent from "react-qr-barcode-scanner";
+import React, { useEffect, useRef, useState } from 'react';
+import Quagga from 'quagga';
 
 const BarcodeScanner = () => {
   const [data, setData] = useState('No se ha escaneado ningún código');
+  const scannerRef = useRef(null);
+
+  useEffect(() => {
+    Quagga.init({
+      inputStream: {
+        name: "Live",
+        type: "LiveStream",
+        target: scannerRef.current,
+        constraints: {
+          width: 640,
+          height: 480,
+          facingMode: "environment" // Usa la cámara trasera
+        },
+      },
+      decoder: {
+        readers: ["code_128_reader", "ean_reader", "ean_8_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader", "i2of5_reader"]
+      }
+    }, (err) => {
+      if (err) {
+        console.error('Error initializing Quagga:', err);
+        return;
+      }
+      Quagga.start();
+    });
+
+    Quagga.onDetected((result) => {
+      setData(result.codeResult.code);
+    });
+
+    return () => {
+      Quagga.stop();
+    };
+  }, []);
 
   return (
     <div style={{ textAlign: 'center' }}>
-      <div style={{ width: '100%', maxWidth: '500px', margin: '0 auto' }}>
-        <BarcodeScannerComponent
-          width={'100%'}
-          height={'100%'}
-          onUpdate={(err, result) => {
-            if (result) {
-              setData(result.text);
-            } else {
-              setData('No se ha escaneado ningún código');
-            }
-          }}
-        />
-      </div>
+      <div ref={scannerRef} style={{ width: '100%', maxWidth: '500px', margin: '0 auto' }} />
       <p>{data}</p>
     </div>
   );
